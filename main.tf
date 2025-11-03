745ad86e6d5c316a74cf79ea2e32ad731b4e3fed
Adegoke Paul <farmerpisco@gmail.com>
2025-11-03 11:29:06 +0000

    Add user data and AWS security group to deploy a single server that returns Hello World upon a request from a client

--- main.tf
+++ main.tf
@@ -3,10 +3,29 @@ provider "aws" {
 }
 
 resource "aws_instance" "test" {
-  ami           = "ami-046c2381f11878233"
-  instance_type = "t3.micro"
+  ami           	 = "ami-046c2381f11878233"
+  instance_type 	 = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
   
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello World" > index.html
+              nohup busybox httpd -f -p 8080 &
+	      EOF
+  user_data_replace_on_change = true
+
   tags = {
-    Name = "test_example"
+    Name = "test-example"
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+   }
 }
