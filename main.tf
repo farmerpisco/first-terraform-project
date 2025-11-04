28ff2891e2f5948193649fc74d5a5fc6e5f92385
Adegoke Paul <farmerpisco@gmail.com>
2025-11-04 09:27:33 +0000

    Updated the code for more efficiency using auto-scaling group, Application Load Balancer and Launch Template

--- main.tf
+++ main.tf
@@ -2,20 +2,131 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_instance" "test" {
-  ami           	 = "ami-046c2381f11878233"
-  instance_type 	 = "t3.micro"
+resource "aws_launch_template" "test" {
+  name_prefix	  	  = "terraform"
+  image_id        	  = "ami-046c2381f11878233"
+  instance_type   	  = "t3.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
-  
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-	      EOF
-  user_data_replace_on_change = true
+ 
+  user_data = base64encode(<<-EOF
+                #!/bin/bash
+                echo "Hello World" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+	        EOF
+    )
 
-  tags = {
-    Name = "test-example"
+  # Required when using a launch configuration with an auto scaling group.
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_lb" "test" {
+  name		     = "terraform-asg-test"
+  load_balancer_type = "application"
+  subnets	     = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.test.arn
+  port		    = 80
+  protocol          = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404. page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type	     = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-test-alb"
+
+  # Allow all outbound requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_autoscaling_group" "test" {
+  launch_template {
+    id = aws_launch_template.test.id
+    version = "$Latest"
+  }
+
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key			= "Name"
+    value		= "terraform-asg-test"
+    propagate_at_launch = true
   }
 }
 
@@ -35,7 +146,7 @@ variable "server_port" {
   type        = number
 }
 
-output "public_ip" {
-  value       = aws_instance.test.public_ip
-  description = "The public IP address of the web server"
+output "alb_dns_name" {
+  value       = aws_lb.test.dns_name
+  description = "The domain name of the load balancer"
 }
